3fd44b246613b3d69780380c311d520962b9fe08
wizzard1994 <matik550@gmal.com>
2018-04-16 12:09:46 +0200

    Final release

--- app/src/main/java/com/example/matik/bookstore/BooksActivity.java
+++ app/src/main/java/com/example/matik/bookstore/BooksActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -26,6 +29,11 @@ public class BooksActivity extends AppCompatActivity
         context.startActivity(starter);
     }
 
+    public static void startCartActivity(Context context) {
+        Intent starter = new Intent(context, CartActivity.class);
+        context.startActivity(starter);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +41,25 @@ public class BooksActivity extends AppCompatActivity
         books = getAllBooksByCategory(getCategoryIdFromExtra());
         initializeRecyclerView();
 
+    }
+
+    @Override //creates new menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.books_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuItemCart:
+                startCartActivity(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private Integer getCategoryIdFromExtra(){
